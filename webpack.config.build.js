cc1f8897a7b2814dd670b6edacd4fb20444aacb6
Anubhav Rana <anubhavrana53@gmail.com>
2024-07-24 00:43:00 +0530

    Update webpack.config.build.js

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -1,12 +1,7 @@
 const path = require('path')
-const { fileURLToPath } = require('url')
 
 const { merge } = require('webpack-merge')
 const config = require('./webpack.config.js')
-
-// const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 module.exports = merge(config, {
     mode: 'production',
@@ -14,8 +9,4 @@ module.exports = merge(config, {
     output: {
         path: path.resolve(__dirname, 'public'),
     },
-
-    // plugins: [
-    //     new CleanWebpackPlugin()
-    // ]
 })
